a2f103b6ab664fc61628e9168e1639c320b41f68
kido5803 <kidosol5803@gmail.com>
2020-12-22 15:32:16 +0800

    Finished

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const nextBtn = document.querySelector('.nextBtn');
-const prevBtn = document.querySelector('prevBtn');
+const prevBtn = document.querySelector('.prevBtn');
 const container = document.querySelector('.images');
 
 let counter = 0;
